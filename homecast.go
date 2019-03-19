85c48189b03f4e09b451c996b39d5ced84fd6bb3
Gilgamesh <32211128+Dontmindmes@users.noreply.github.com>
2019-03-18 18:04:47 -0700

    Update homecast.go

--- homecast.go
+++ homecast.go
@@ -43,11 +43,9 @@ func (g *CastDevice) Speak(ctx context.Context, text, lang string) error {
 	return g.Play(ctx, url)
 }
 
-func (g *CastDevice) SetVolume(ctx context.Context, volume float64)  {
+func (g *CastDevice) SetVolume(ctx context.Context, volume float64) error {
 	_, err = g.client.Receiver().SetVolume(ctx, &controllers.Volume{Level: &volume})
-	if err != nil {
-		fmt.Println(err)
-	}
+	return err
 }
 // Play plays media contents on cast device
 func (g *CastDevice) Play(ctx context.Context, url *url.URL) error {
